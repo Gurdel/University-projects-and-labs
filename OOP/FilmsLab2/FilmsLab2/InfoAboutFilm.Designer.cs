834d47d154ff83854781b66bde57c158b79a8133
Maksym Shevchenko <50591739+Gurdel@users.noreply.github.com>
2020-11-29 21:41:56 +0200

    add OOP projects
    SQL, Entity Framework, MVC, XML, UML + database form application

---
+++ OOP/FilmsLab2/FilmsLab2/InfoAboutFilm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FilmsLab2
+{
+    partial class InfoAboutFilm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblActors = new System.Windows.Forms.Label();
+            this.lblProducer = new System.Windows.Forms.Label();
+            this.lblGenres = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.rtbGenres = new System.Windows.Forms.RichTextBox();
+            this.rtbActors = new System.Windows.Forms.RichTextBox();
+            this.rtbProducer = new System.Windows.Forms.RichTextBox();
+            this.rtbYear = new System.Windows.Forms.RichTextBox();
+            this.rtbName = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblActors
+            // 
+            this.lblActors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblActors.AutoSize = true;
+            this.lblActors.Location = new System.Drawing.Point(12, 216);
+            this.lblActors.Name = "lblActors";
+            this.lblActors.Size = new System.Drawing.Size(84, 13);
+            this.lblActors.TabIndex = 19;
+            this.lblActors.Text = "Список акторів";
+            // 
+            // lblProducer
+            // 
+            this.lblProducer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblProducer.AutoSize = true;
+            this.lblProducer.Location = new System.Drawing.Point(12, 64);
+            this.lblProducer.Name = "lblProducer";
+            this.lblProducer.Size = new System.Drawing.Size(52, 13);
+            this.lblProducer.TabIndex = 18;
+            this.lblProducer.Text = "Режисер";
+            // 
+            // lblGenres
+            // 
+            this.lblGenres.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGenres.AutoSize = true;
+            this.lblGenres.Location = new System.Drawing.Point(12, 90);
+            this.lblGenres.Name = "lblGenres";
+            this.lblGenres.Size = new System.Drawing.Size(81, 13);
+            this.lblGenres.TabIndex = 17;
+            this.lblGenres.Text = "Список жанрів";
+            // 
+            // lblYear
+            // 
+            this.lblYear.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(12, 38);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(22, 13);
+            this.lblYear.TabIndex = 16;
+            this.lblYear.Text = "Рік";
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 12);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 13);
+            this.lblName.TabIndex = 15;
+            this.lblName.Text = "Назва";
+            // 
+            // rtbGenres
+            // 
+            this.rtbGenres.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbGenres.Location = new System.Drawing.Point(116, 90);
+            this.rtbGenres.Name = "rtbGenres";
+            this.rtbGenres.ReadOnly = true;
+            this.rtbGenres.Size = new System.Drawing.Size(672, 120);
+            this.rtbGenres.TabIndex = 14;
+            this.rtbGenres.Text = "";
+            // 
+            // rtbActors
+            // 
+            this.rtbActors.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbActors.Location = new System.Drawing.Point(116, 216);
+            this.rtbActors.Name = "rtbActors";
+            this.rtbActors.ReadOnly = true;
+            this.rtbActors.Size = new System.Drawing.Size(672, 120);
+            this.rtbActors.TabIndex = 13;
+            this.rtbActors.Text = "";
+            // 
+            // rtbProducer
+            // 
+            this.rtbProducer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbProducer.Location = new System.Drawing.Point(116, 64);
+            this.rtbProducer.Name = "rtbProducer";
+            this.rtbProducer.ReadOnly = true;
+            this.rtbProducer.Size = new System.Drawing.Size(672, 20);
+            this.rtbProducer.TabIndex = 12;
+            this.rtbProducer.Text = "";
+            // 
+            // rtbYear
+            // 
+            this.rtbYear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbYear.Location = new System.Drawing.Point(116, 38);
+            this.rtbYear.Name = "rtbYear";
+            this.rtbYear.ReadOnly = true;
+            this.rtbYear.Size = new System.Drawing.Size(672, 20);
+            this.rtbYear.TabIndex = 11;
+            this.rtbYear.Text = "";
+            // 
+            // rtbName
+            // 
+            this.rtbName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbName.Location = new System.Drawing.Point(116, 12);
+            this.rtbName.Name = "rtbName";
+            this.rtbName.ReadOnly = true;
+            this.rtbName.Size = new System.Drawing.Size(672, 20);
+            this.rtbName.TabIndex = 10;
+            this.rtbName.Text = "";
+            // 
+            // InfoAboutFilm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 347);
+            this.Controls.Add(this.lblActors);
+            this.Controls.Add(this.lblProducer);
+            this.Controls.Add(this.lblGenres);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.rtbGenres);
+            this.Controls.Add(this.rtbActors);
+            this.Controls.Add(this.rtbProducer);
+            this.Controls.Add(this.rtbYear);
+            this.Controls.Add(this.rtbName);
+            this.Name = "InfoAboutFilm";
+            this.Text = "Детальна інформація про фільм";
+            this.Load += new System.EventHandler(this.InfoAboutFilm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblActors;
+        private System.Windows.Forms.Label lblProducer;
+        private System.Windows.Forms.Label lblGenres;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.RichTextBox rtbGenres;
+        private System.Windows.Forms.RichTextBox rtbActors;
+        private System.Windows.Forms.RichTextBox rtbProducer;
+        private System.Windows.Forms.RichTextBox rtbYear;
+        private System.Windows.Forms.RichTextBox rtbName;
+    }
+}
